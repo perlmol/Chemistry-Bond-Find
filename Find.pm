24f69b1fd572d03f9259238ffb7d5370ae7e9e71
Ivan Tubert-Brohman <ivan.tubert@gmail.com>
2004-05-11 21:37:51 +0000

    Added recursive partitioning algorithm. Release 0.05

--- Find.pm
+++ Find.pm
@@ -1,5 +1,7 @@
 package Chemistry::Bond::Find;
+
 $VERSION = '0.05';
+# $Id$
 
 =head1 NAME
 
@@ -9,10 +11,21 @@ Chemistry::Bond::Find - Detect bonds in a molecule from atomic 3D coordinates
 
     use Chemistry::Bond::Find 'find_bonds';
 
-    find_bonds($mol, order => 1);
+    find_bonds($mol, %options);
+
+=head1 DESCRIPTION
+
+Detects the bonds in a molecule from its 3D coordinates by using simple
+cutoffs.  The current version does not guess the bond orders; all bonds will
+have a bond order of 1.
+
+This module is part of the PerlMol project, L<http://www.perlmol.org/>.
+
+=head1 FUNCTIONS
+
+=over
 
 =cut
-
 
 use strict;
 use Chemistry::Mol;
@@ -20,15 +33,6 @@ use Exporter;
 
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(find_bonds);
-
-=head1 DESCRIPTION
-
-Detect the bonds in a molecule from its 3D coordinates by using
-simple cutoffs. Currently uses a silly brute-force, N^2 algorithm, so
-it's probably not a good idea to run it on a protein.
-
-=cut
-
 
 # table taken from
 # http://environmentalchemistry.com/yogi/periodic/covalentradius.html
@@ -50,40 +54,19 @@ my %Covalent_Radius = (
     Yb => 1.74, Zn => 1.25, Zr => 1.45,
 );
 
-my $Tolerance = 0.3; # as a fraction
-my $Default_Radius = 1.5;
+my $Default_Radius = 1.5; # radius for unknown elements
 
-my %Cutoff; # Table of cutoffs. Example: key = "H C", value = [0.76, 1.42]
-
+# I considered inlining this function, but the performance gain was minimal
+# ( < 5 % ), so it's probably better to leave it here
+# $opts->{cuttof} Hash Table of cutoffs. 
+# Example: key = "H C", value = [0.76, 1.42]
 sub are_bonded {
-    my ($a, $b, $r) = @_;
-    my ($min, $max);
-    my $pair = "$a $b";
-    if($Cutoff{$pair}) {
-        ($min, $max) = @{$Cutoff{$pair}};
-    } else {
-        my $bond_r = ($Covalent_Radius{$a} || $Default_Radius) 
-                + ($Covalent_Radius{$b} || $Default_Radius);
-        $min = $bond_r * (1-$Tolerance);
-        $max = $bond_r * (1+$Tolerance);
-        $Cutoff{$pair} = [$min, $max];
-    }
-    return ($r > $min and $r < $max);
+    my ($a, $b, $r, $opts) = @_;
+    return $r < ($opts->{cuttoffs}{"$a $b"} ||= 
+        (($Covalent_Radius{$a} || $Default_Radius) 
+         + ($Covalent_Radius{$b} || $Default_Radius)) * $opts->{tolerance});
 }
 
-
-sub find_bonds {
-    my ($mol, %opts) = @_;
-    my @a = $mol->atoms;
-    for (my $i = 0; $i < @a; ++$i) {
-	for (my $j = $i + 1; $j < @a; ++$j) {
-	    my ($a1, $a2) = ($a[$i], $a[$j]);
-	    if (are_bonded($a1->symbol, $a2->symbol, $a1->distance($a2))) {
-		$mol->add_bond(Chemistry::Bond->new(atoms=>[$a1, $a2]));
-	    }
-	}
-    }
-}
 
 # Future work:
 # assign hybridization
@@ -116,18 +99,151 @@ sub assign_orders {
 
 }
 
+
+
+=item find_bonds($mol, %options)
+
+Find and add the bonds in a molecule. Only use it in molecules that have no 
+explicit bonds; for example, after reading a file with 3D coordinates but no
+bond orders.
+
+Available options:
+
+=over
+
+=item tolerance
+
+Defaults to 1.1. Two atoms are considered to be bound if the distance between
+them is less than the sum of their covalent radii multiplied by the tolerance.
+
+=item margin
+
+NOTE: in general setting this option is not recommended, unless you know what
+you are doing. Used by the recursive partitioning algorithm when "stitching"
+the interfaces between partitions. It defaults to 2 * Rmax * tolerance, where
+Rmax is the largest covalent radius among the elements found in the molecule.
+For example, if a molecule has C, H, N, O, and I, Rmax = R(I) = 1.33, so the
+margin defaults to 2 * 1.33 * 1.1 = 2.926. This margin ensures that no bonds
+are missed by the partitioning algorithm. Using a smaller value gives faster
+results, but at the risk of missing some bonds. In this exaple, if you are
+certain that your molecule doesn't contain I-I bonds (but it has C-I bonds),
+you can set margin to (0.77 + 1.33) * 1.1 = 2.31 and you still won't miss any
+bonds. All this only has a real impact for molecules with a thousand atoms or
+more.
+
+=item min_atoms
+
+Defaults to 20. Partitions with fewer that min_atoms will not be partitioned
+further, but their bonds will be found by a simple n^2 method. The default
+value seems to be optimal, but you can play with it if you want.
+
+=back
+
+=cut
+
+sub find_bonds {
+    my ($mol, %opts) = @_;
+    %opts = (min_atoms => 20, tolerance => 1.1, %opts,
+        cutoffs => {});  # set defaults
+    my $margin = guess_margin($mol, \%opts);    
+    %opts = (margin => $margin, %opts);
+    _partition($mol, [$mol->atoms], 0, \%opts);
+}
+
+sub guess_margin {
+    my ($mol, $opts) = @_;
+    my $formula = $mol->formula_hash;
+    my $max = 0;
+    for my $elem (keys %$formula) {
+        $max = $Covalent_Radius{$elem} if $Covalent_Radius{$elem} > $max;
+    }
+    $max *= 2 * $opts->{tolerance};
+    #printf "MARGIN guessed at (%.2f)\n", $max;
+    $max;
+}
+
+sub _partition {
+    my ($mol, $atoms, $dir, $opts) = @_;
+
+    #printf "_partition(%s, $dir)\n", scalar(@$atoms);
+
+    if (@$atoms < $opts->{min_atoms}) {
+        #print "BOTTOM!\n";
+        find_bonds_n2_one_set($mol, $atoms, $opts);
+        return;
+    }
+
+    my $min = ($atoms->[0]->coords->array)[$dir];
+    my $max = $min;
+    my $center;
+    for my $atom (@$atoms) {
+        my $coord = ($atom->coords->array)[$dir]; 
+        $center += $coord;
+        $min = $coord if $coord < $min;
+        $max = $coord if $coord > $min;
+    }
+    $center /= @$atoms;
+    #printf "center($dir)=%.2f; range=(%.2f, %.2f)\n", $center, $min, $max;
+
+    my @left = grep { ($_->coords->array)[$dir] < $center } @$atoms;
+    my @right = grep { ($_->coords->array)[$dir] >= $center } @$atoms;
+    _partition($mol, \@left, ($dir + 1) % 3, $opts);
+    _partition($mol, \@right, ($dir + 1) % 3, $opts);
+
+    # merge the interface between the two halves
+    my $margin = $opts->{margin};
+    my @left_margin = 
+        grep { ($_->coords->array)[$dir] > $center - $margin } @left;
+    my @right_margin = 
+        grep { ($_->coords->array)[$dir] < $center + $margin } @right;
+    find_bonds_n2_two_sets($mol, \@left_margin, \@right_margin, $opts);
+}
+
+sub find_bonds_n2_one_set {
+    my ($mol, $atoms, $opts) = @_;
+    for (my $i = 0; $i < @$atoms; ++$i) {
+	for (my $j = $i + 1; $j < @$atoms; ++$j) {
+	    my ($a1, $a2) = ($atoms->[$i], $atoms->[$j]);
+	    if (are_bonded($a1->symbol, $a2->symbol, scalar $a1->distance($a2), $opts)) {
+		$mol->add_bond(Chemistry::Bond->new(atoms=>[$a1, $a2]));
+	    }
+	}
+    }
+}
+
+sub find_bonds_n2_two_sets {
+    my ($mol, $atoms1, $atoms2, $opts) = @_;
+    for my $a1 (@$atoms1) {
+	for my $a2 (@$atoms2) {
+	    if (are_bonded($a1->symbol, $a2->symbol, scalar $a1->distance($a2), $opts)) {
+		$mol->add_bond(Chemistry::Bond->new(atoms=>[$a1, $a2]));
+	    }
+	}
+    }
+}
+
 1;
 
-=head1 SEE ALSO
-
-Chemistry::Mol, Chemistry::Atom, Chemistry::Bond
-
-=head1 AUTHOR
-
-Ivan Tubert-Brohman <ivan@tubert.org>
+=back
 
 =head1 VERSION
 
-$Id$
+0.05
+
+=head1 SEE ALSO
+
+L<Chemistry::Mol>, L<Chemistry::Atom>, L<Chemistry::Bond>,
+L<http://www.perlmol.org/>.
+
+=head1 AUTHOR
+
+Ivan Tubert E<lt>itub@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004 Ivan Tubert. All rights reserved. This program is free
+software; you can redistribute it and/or modify it under the same terms as
+Perl itself.
 
 =cut
+
--- testf.pl
+++ testf.pl
@@ -4,12 +4,16 @@ use strict;
 use Chemistry::Mol;
 use Chemistry::File::Mopac;
 use Chemistry::File::MDLMol;
+use Chemistry::File::PDB;
 use blib;
 use Chemistry::Bond::Find 'find_bonds';
 
-my $mol = Chemistry::Mol->read($ARGV[0] || "methane.mop");
+my $mol = Chemistry::Mol->read($ARGV[0] || "test750.pdb");
 #print $mol->formula, "\n";
 
-find_bonds($mol);
+#find_bonds($mol);
 #for my $bond ($mol->bonds) { print $bond, ": ", $bond->atoms, "\n"; }
-print $mol->print(format => "mdl");
+#print $mol->print(format => "mdl");
+
+Chemistry::Bond::Find::find_bonds($mol);
+$mol->write("rec.mol", format => 'mdl');
